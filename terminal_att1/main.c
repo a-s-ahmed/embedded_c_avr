27cfc711a4537b892680837a31635529b14f2d9e
Ahmed Ahmed <ahmed.ahmed@lightsource.ca>
2020-09-02 07:46:26 -0600

    added simple terminal - need to dive deeper into the docs for more ideas and something with more IO and i2c/spi bus usage

---
+++ terminal_att1/main.c
@@ -0,0 +1,30 @@
+#include <avr/io.h>
+#define BAUD 0x33
+
+
+void initialize_usart_serial(void){
+    //set baud rate to 19200 and init our serial comms
+    UCSR0B = (1<<TXEN0);
+    UCSR0C = (1<<UCSZ01) | (1<<UCSZ00);
+    UBRR0L = BAUD;
+}
+
+void usart_send(unsigned char letter){
+    //send letter across usart
+    while (!(UCSR0A & (1<<UDRE0)));
+    UDR0 = letter;
+}
+
+int main(void){
+    //Continuously send Hello World!
+    unsigned char str[30] = "Hello World";
+    unsigned char strLength = 13;
+    unsigned char i = 0;
+
+    initialize_usart_serial();
+
+    while (1){
+        usart_send(str[i++]);
+        if (i >= strLength) i = 0;
+    }
+}
